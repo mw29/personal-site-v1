4779c74a6c6ec7fefb1be1b54a339dc04d4d27bc
mw29 <madeline.chloe429@gmail.com>
2022-07-16 11:35:44 -0600

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header(){
+    return(
+        <header>
+            <h2>Header Test</h2>
+            <NavBar/>
+        </header>
+    );
+}
+
+export default Header;
