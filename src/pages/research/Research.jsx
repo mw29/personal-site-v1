1a1be709eb801c40a94bdd713ae9682ed93312d9
mw29 <madeline.chloe429@gmail.com>
2022-07-20 16:40:20 -0600

    cos

--- src/pages/research/Research.jsx
+++ src/pages/research/Research.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Research.css';
 
 function Research() {
     return (
@@ -6,23 +7,51 @@ function Research() {
             <br />
             <h1>Research</h1>
             <br />
-            <nav id="research-scroll" className="navbar navbar-inverse navebar-fixed-top">
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#research1-heading">First</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#research2-heading">Second</a>
-                    </li>
-                </ul>
-            </nav>
-            <div data-bs-spy="scroll" data-bs-target="#research-scroll" data-bs-root-margin="0px 0px">
-                <h4 id="research1-heading">First heading</h4>
-                <p>jihugfdfhgjkljihugyftdrsetdfyguhijlokiugyftdrstrdtfghjko;jihugy</p>
-                <br/>
-                <h4 id="research2-heading">Second heading</h4>
-                <p>...</p>
+            
+            <div className="p-3 border">
+                <div className="card-body">
+                    <h3>
+                        Geometry of Learning
+                    </h3>
+                    <h6 className="research-project-list">list of tools</h6>
+                    <h5>Description of project</h5>
 
+                </div>
+            </div>
+            <br/>
+            <div className="p-3 border">
+                <div className="card-body">
+                    <h3>
+                        Microgesture Dataset
+                    </h3>
+                    <h6 className="research-project-list">list of tools</h6>
+                    <h5>Description of project</h5>
+
+                </div>
+            </div>
+            <br/>
+            <div className="p-3 border">
+                <div className="card-body">
+                    <h3>
+                        Microgesture Elicitation
+                    </h3>
+                    <h6 className="research-project-list">list of tools</h6>
+                    <p>I was tasked with thoroughly reviewing the state-of-the-art research, designing and implementing a study, and collectively analyzing the results. I became extremely curious about how microgesture interaction compares to mid-air interaction in an AR environment, specifically in the context of manipulating 3D objects or completing a sequential multi-step task. I designed an elicitation study to investigate the comparison of microgestures and mid-air gestures across a multitude of AR interfaces.</p>
+
+
+                </div>
+            </div>
+            <br/>
+            <div className="p-3 border">
+                <div className="card-body">
+                    <h3>
+                        AR/VR Data Visualization
+                    </h3>
+                    <h6 className="research-project-list">list of tools</h6>
+                    <p>I implemented interactive data visualizations, created and optimized ways to update data sources in real-time, developed spatial anchors, configured a Photon server to host the project, optimized the server connection, connected input devices (keyboard, VR pen), configured HTC Vive sensors to work with a HoloLens 2, helped with fundamental experiment design decisions, and provided feedback on the project\'s implementation process. </p>
+
+
+                </div>
             </div>
         </div>
 
