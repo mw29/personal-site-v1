4c0191557c63a3db88d9a8cbd41623be633bafdb
mw29 <madeline.chloe429@gmail.com>
2022-09-08 11:54:31 -0600

    research

--- src/pages/research/Research.jsx
+++ src/pages/research/Research.jsx
@@ -1,61 +1,69 @@
 import React from "react";
 import "./Research.css";
 
-function Research(){
-    return(
+function Research() {
+    return (
         <div className="container">
-            <br/>
+            <br />
             <h1>Research</h1>
-            <br/>
-            <br/>
+            
+            <h5 className="subheading"><em>Publication links coming soon!</em></h5>
+            
+            <br />
             <div className="row">
                 <div className="col">
-                    <h3>Geometry of Learning</h3>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">Geometry of Learning</h3>
+                            <h5 className="card-text">I worked with a team of math and computer science professors to developed state-of-the-art adversarial attack defences using manifold learning, dimension estimation, and other advanced mathematical tools.</h5>
+                        </div>
+                    </div>
                 </div>
-                
+        
                 <div className="col">
-                    <h5>I worked with a team of math and computer science professors to developed state-of-the-art adversarial attack defences using manifold learning, dimension estimation, and other advanced mathematical tools.</h5>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">Microgesture Dataset</h3>
+                            <h5 className="card-text">Managed a project that explored machine learning applications of microgestures. We created the first public microgesture dataset, consisting of both synthetic and real data. We used the dataset to train neural networks to recognize these gestures.</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <br/>
+            <br />
             <div className="row">
                 <div className="col">
-                    <h3>Microgesture Dataset</h3>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">Gestural Interaction Techniques</h3>
+                            <h5 className="card-text">Research comparing microgestures and mid-air interaction techniques in augmented reality, focus on usability and accessibility.</h5>
+                        </div>
+                    </div>
                 </div>
-                
+
+            <br />
+            
                 <div className="col">
-                    <h5>Created the first public microgesture dataset, consisting of both synthetic and real data. The dataset was used to train gesture recognition models.</h5>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">Biomass in Virtual Environment</h3>
+                            <h5 className="card-text">Analyzed data, produced statistical results, and contributed to paper examining the effects of biomass in virtual environments.</h5>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <br/>
-            <div className="row">
+
+            <br />
+            
                 <div className="col">
-                    <h3>Gestural Interaction Techniques</h3>
-                </div>
-                
-                <div className="col">
-                    <h5>Research comparing microgestures and mid-air interaction techniques in augmented reality</h5>
-                </div>
-            </div>
-            <br/>
-            <div className="row">
-                <div className="col">
-                    <h3>Biomass in Virtual Environment</h3>
-                </div>
-                <div className="col">
-                    <h5>Analyzed data, produced statistical results, and contributed to paper examining the effects of biomass in virtual environments</h5>
-                </div>
-            </div>
-            <br/>
-            <div className="row">
-                <div className="col">
-                    <h3>Immersive Analytics and Data Visualization</h3>
-                </div>
-                <div className="col">
-                    <h5>Collaborated on a research project to better understand human interaction with immersive analytics in an augmented and virtual reality environment</h5>
+                <div className="card">
+                        <div className="card-body">
+                    <h3 className="card-title">Immersive Analytics and Data Visualization</h3>
+
+                    <h5 className="card-text">Collaborated on a research project to better understand human interaction with immersive analytics in an augmented and virtual reality environment.</h5>
                 </div>
             </div>
         </div>
+            </div >
+        </div >
     );
 }
 
