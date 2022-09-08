da5544eb6359f6afc588f6591500c4d3307e6f0e
mw29 <madeline.chloe429@gmail.com>
2022-09-08 14:23:46 -0600

    changesssssss

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-function Contact(){
-    return(
+function Contact() {
+    return (
         <div>
-            <br/>
+            <br />
             <h1>Contact Me</h1>
+            <br />
+            <br />
+            <div className="row">
+                <div className="col">
+                    <h3><span className="dif">Phone:</span> (303) 834-5649</h3>
+                    <h3><span className="dif">Email:</span> madeline.chloe429@gmail.com</h3>
+                </div>
+            </div>
         </div>
     );
 }
